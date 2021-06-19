eaa1177a58bddbd2b42c112678ae85256203492d
Antoneat <antonellatavarone1996@gmail.com>
2021-06-19 10:33:04 -0500

    interruptor1 y 2 script merged, correcion en player tags

--- Assets/Scripts/interruptores/Interruptor.cs
+++ Assets/Scripts/interruptores/Interruptor.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interruptor2 : MonoBehaviour
+public class Interruptor : MonoBehaviour
 {
-   // public BoxCollider boxC;
+    //public BoxCollider boxC;
     modelChange modCha;
-
+    
     void Start()
     {
         modCha = GetComponent<modelChange>();
     }
 
-
+ 
     void Update()
     {
-
+        
     }
 
     void OnTriggerEnter(UnityEngine.Collider other)
@@ -24,8 +24,7 @@ public class Interruptor2 : MonoBehaviour
         {
             modCha.model1.SetActive(false);
             modCha.model2.SetActive(true);
-            //boxC.size = new Vector3(6, 4, 2);
-            //boxC.center= new Vector3(0, 0.25f, 0); //box collider se hace de la mitad del size, cambia size en y
+            
         }
     }
 }
